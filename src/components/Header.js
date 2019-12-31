c8b4552f7a7434c26f605b8e39d8f8ad78959ba6
kyle <crokadilekyle@gmail.com>
2019-12-31 17:10:19 -0500

    got started with styling with css modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import headerStyles from "./modules/header.module.css"
 import { useStaticQuery, Link, graphql } from 'gatsby'
 
 export default () => {
@@ -19,22 +20,28 @@ export default () => {
 
     return (
         <header>
-            <ul>
-                <Link to='/' ><li>Twin Web Development</li></Link>
+            <nav className={headerStyles.nav}>
+                <ul className={headerStyles.navlinks}>
+                    <li className={headerStyles.navbrand}><Link className={headerStyles.navlink} to='/' >Twin Web Development</Link></li>
 
-                {data.allWordpressMenusMenusItems.nodes.map((node) => (
+                    {data.allWordpressMenusMenusItems.nodes.map((node) => (
 
-                    node.items.map((item) => (
-                        <li><Link to={item.slug === null ? item.title.replace(" ", "-").toLowerCase() : item.slug}>{item.title}</Link></li>
-                    ))
+                        node.items.map((item) => (
+                            <li><Link
+                                className={headerStyles.navlink}
+                                to={item.slug === null ?
+                                    item.title.replace(" ", "-").toLowerCase()
+                                    :
+                                    item.slug}>
+                                {item.title}
+                            </Link>
+                            </li>
+                        ))
 
-                ))}
-
-                {/* {data.allWordpressMenusMenusItems.nodes.items.map(({ item }) => (
-                    <li><Link to={item.url}>{item.title}</Link></li>
-                ))} */}
-            </ul>
-        </header>
+                    ))}
+                </ul>
+            </nav>
+        </header >
     )
 }
 
